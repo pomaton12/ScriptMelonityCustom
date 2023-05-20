a725d63c9f0bf10e360fd0481492fa1a3be42e31
pomaton12 <129473476+pomaton12@users.noreply.github.com>
2023-05-20 08:45:03 -0500

    Update WindrunnerScript.js

--- WindrunnerScript.js
+++ WindrunnerScript.js
@@ -1,1 +1,116 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
 
+/***/ "./src/AutoSaverWindrunner.ts":
+/*!**********************************!*\
+  !*** ./src/AutoSaverWindrunner.ts ***!
+  \**********************************/
+/***/ (() => {
+
+eval(`
+	// Definición del objeto AutoSaverAlchemist
+	const AutoSaverWindrunner = {};
+
+	// Declaración de la variable localHero
+	let localHero;
+
+	// Definición del array path_
+	const path_ = ['Heroes', 'Intelligence', 'Windranger'];
+
+	// Creación del toggle isUiEnabled
+	let isUiEnabled = Menu.AddToggle(path_, 'Gale Force Use', true);
+	isUiEnabled.SetImage('panorama/images/spellicons/windrunner_gale_force_png.vtex_c');
+
+	// Definición de la función OnUpdate
+	let previousEnemyPositions = {};
+	//===============================
+	AutoSaverWindrunner.OnUpdate = () => {
+	  if (localHero && isUiEnabled) {
+	    if (localHero.GetUnitName() !== "npc_dota_hero_windrunner")
+	      return;
+	    const modifiers = localHero.GetModifiers();
+	    for (let modifier of modifiers) {
+	      if (modifier.GetName() === 'modifier_windrunner_focusfire') {
+		const remainingTime = modifier.GetRemainingTime();
+		if (remainingTime <= 20) {
+		// Nueva condición para activar winrun siempre
+			let windrun = localHero.GetAbilityByIndex(2);
+			if (windrun && windrun.IsExist() && windrun.CanCast()) {
+			  windrun.CastNoTarget();
+			}
+		  // Nueva condición para activar BKB si el enemigo tiene activado Blade Mail
+		  let enemies = localHero.GetHeroesInRadius(1000, Enum.TeamType.TEAM_ENEMY);
+		  for (let enemy of enemies) {
+		    if (enemy.HasModifier("modifier_item_blade_mail_reflect")) {
+		      let bkb = localHero.GetItem('item_black_king_bar', true);
+		      if (bkb && bkb.CanCast()) {
+			bkb.CastNoTarget();
+		      }
+		    }
+		  }
+
+		  let gale_force = localHero.GetAbilityByIndex(3);
+		  if (gale_force && gale_force.IsExist() && gale_force.CanCast()) {
+		    enemies = localHero.GetHeroesInRadius(1000, Enum.TeamType.TEAM_ENEMY);
+		    for (let enemy of enemies) {
+		      let enemyId = enemy.GetPlayerID();
+		      let isAttacking = enemy.IsAttacking() && enemy.GetAttackTarget() === localHero;
+		      let currentPosition = enemy.GetAbsOrigin();
+		      let isEscaping = false;
+
+		      if (previousEnemyPositions[enemyId]) {
+			let previousPosition = previousEnemyPositions[enemyId];
+			let distanceMoved = currentPosition.sub(previousPosition).Length2D();
+			isEscaping = distanceMoved > 0 && !isAttacking;
+		      }
+
+		      previousEnemyPositions[enemyId] = currentPosition;
+
+		      let vec1 = localHero.GetAbsOrigin();
+		      let vec2 = enemy.GetAbsOrigin();
+		      let distance = vec1.sub(vec2).Length2D();
+
+		      if (distance <= 1000) {
+			let pushDirection;
+			if (isAttacking) {
+			  pushDirection = vec2.sub(vec1).Normalized();
+			} else if (isEscaping) {
+			  pushDirection = vec1.sub(vec2).Normalized();
+			} else {
+			  continue;
+			}
+			gale_force.CastPosition(vec1.add(pushDirection));
+		      }
+		    }
+		  }
+		}
+	      }
+	    }
+	  }
+	};
+	// Definición de la función OnScriptLoad
+	AutoSaverWindrunner.OnScriptLoad = AutoSaverWindrunner.OnGameStart = () => {
+		localHero = EntitySystem.GetLocalHero();
+	};
+
+	// Definición de la función OnGameEnd
+	AutoSaverWindrunner.OnGameEnd = () => {
+		localHero = null;
+	};
+
+	// Registro del script
+	RegisterScript(AutoSaverWindrunner);
+
+`);
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/AutoSaverWindrunner.ts"]();
+/******/ 	
+/******/ })()
+;
